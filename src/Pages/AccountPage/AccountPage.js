392b7c0d42d39d8e7e31e217234985bcfb51d3fe
kamryn-fey <kamrynfey12@gmail.com>
2022-04-08 11:41:59 -0500

    Account update

--- src/Pages/AccountPage/AccountPage.js
+++ src/Pages/AccountPage/AccountPage.js
@@ -6,47 +6,47 @@ import { useHistory } from "react-router-dom";
 
 
 function getAccountInformation() {
-	
-return (
-<positioning>
-  <div class="container">
-    <div class="title">Profile</div>
-    <div class="content">
-      <form action="#">
-        <div class="user-details">
-          <div class="input-box">
-            <span class="details"><strong>Full Name</strong></span>
-            <input type="text" required></input>
-          </div>
-          <div class="input-box">
-            <span class="details"><strong>Username</strong></span>
-            <input type="text"required></input>
-          </div>
-          <div class="input-box">
-            <span class="details"><strong>Email</strong></span>
-            <input type="text" required></input>
-          </div>
-          <div class="input-box">
-            <span class="details"> <strong>Phone Number</strong></span>
-            <input type="text" required></input>
-          </div>
-          <div class="input-box">
-            <span class="details"><strong>Password</strong></span>
-            <input type="text" required></input>
-          </div>
-          <div class="input-box">
-            <span class="details"><strong>Confirm Password</strong></span>
-            <input type="text" required></input>
-          </div>
-        </div>
-        <div class="button">
-          <input type="submit" value="Profile"></input>
-        </div>
-      </form>
-    </div>
-	</div>
-	</positioning>
-);
 
-	}
+  return (
+    <body>
+        <div class="container">
+          <div class="title">Profile</div>
+          <div class="content">
+            <form action="#">
+              <div class="user-details">
+                <div class="input-box">
+                  <span class="details"><strong>First Name</strong></span>
+                  <input type="text" required></input>
+                </div>
+                <div class="input-box">
+                  <span class="details"><strong>Last Name</strong></span>
+                  <input type="text" required></input>
+                </div>
+                <div class="input-box">
+                  <span class="details"><strong>Username</strong></span>
+                  <input type="text" required></input>
+                </div>
+                <div class="input-box">
+                  <span class="details"><strong>Email</strong></span>
+                  <input type="text" required></input>
+                </div>
+                <div class="input-box">
+                  <span class="details"> <strong>Phone Number</strong></span>
+                  <input type="text" required></input>
+                </div>
+                <div class="input-box">
+                  <span class="details"><strong>Address</strong></span>
+                  <input type="text" required></input>
+                </div>
+              </div>
+              <div class="button">
+                <input type="submit" value="Profile"></input>
+              </div>
+            </form>
+          </div>
+        </div>
+        </body>
+  );
+
+}
 export default getAccountInformation;
